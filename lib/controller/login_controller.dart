85ab522da8853d6760101c9c471ac84b1fb21154
vikas gupta <vikank9891@gmail.com>
2021-08-27 11:43:16 +0530

    Login complete.

--- lib/controller/login_controller.dart
+++ lib/controller/login_controller.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:flutter/material.dart';
@@ -15,13 +16,19 @@ class LoginController extends GetxController{
     super.onInit();
   }
 
-  void login(){
-    if (usernameController.text != '' && passwordController.text != '') {
-      print('Successfull');
+  void login() async{
+    Future<QuerySnapshot> data = FirebaseFirestore.instance
+        .collection('users')
+        .where('email', isEqualTo: usernameController.text)
+        .where('password', isEqualTo: passwordController.text)
+        .get();
+    QuerySnapshot response = await (data);
+
+    if (response.docs.isNotEmpty) {
       userdata.write('isLogged', true);
       userdata.write('username',usernameController.text);
       Get.offAll(Home());
-    }else{
+    } else{
       Get.snackbar("Error", "Please Enter Username & Password",snackPosition: SnackPosition.BOTTOM);
     }
   }
